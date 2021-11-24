b4fba97552a955821b3b21c8478e87cd15039ad5
senjames <ujames41@gmail.com>
2021-11-24 19:48:52 +0100

    updatedLoading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import FirstScreen from "./components/FirstScreen";
@@ -6,15 +6,49 @@ import SecondScreen from "./components/SecondScreen";
 import Home from "./Pages/Home";
 
 function App() {
+  const [landing, setLanding ] =  useState(false)
+  const [secondLand, setSecondLand ] = useState(false)
+  const [seconds, setSeconds ] = useState(8)
+
+
   useEffect(() =>{
+
+    const timer = seconds > 0 && setInterval(() => setSeconds(seconds - 1), 1000);
+    if(seconds === 5){
+      setLanding(true)
+    }
+    else if(seconds === 0){
+      setSecondLand(true)
+      // setSecondLand(false)
+    }
     
-  }, [])
-  return (
-    <BrowserRouter>
-      
-      <Home />
-    </BrowserRouter>
-  );
+    return () => clearInterval(timer);
+    
+  }, [seconds])
+
+  if(landing === false){
+    return (
+      <BrowserRouter>
+        <FirstScreen />
+      </BrowserRouter>
+    )
+  }
+  else if(landing === true && secondLand === false){
+    return (
+      <BrowserRouter>
+        <SecondScreen />
+      </BrowserRouter>
+    )
+  }
+  else if(landing === true && secondLand === true){
+
+    return (
+      <BrowserRouter>
+        
+        <Home />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/components/SecondScreen.js
+++ src/components/SecondScreen.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import logo from '../images/logo2.png'
+import people from "../images/background3.png";
 const SecondScreen = () => {
     return (
       <div className="secondscreen">
-       <div className='secondscreen__image'>
-        <img src={logo} alt="Logo" />
-        </div>
-        <div className="secondscreen__background"></div>
+       {/* <div className='secondscreen__image'> */}
+          <img className='secondscreen__image' src={logo} alt="Logo" />
+        {/* </div> */}
+        {/* <div className="secondscreen__background"> */}
+          <img className="secondscreen__background" src={people} alt="Logo" />
+        {/* </div> */}
       </div>
     );
 }
